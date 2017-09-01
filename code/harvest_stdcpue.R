7b7690ca4224191c3f533fb53835f89a97ce697f
Katie Palof <katie.palof@alaska.gov>
2017-09-01 13:44:22 -0800

    add data file with stat area for each survey area.  Bring these in to merge and create data set with cumulative pots

--- code/harvest_stdcpue.R
+++ code/harvest_stdcpue.R
@@ -21,15 +21,22 @@ theme_set(theme_bw(base_size=12,base_family='Times New Roman')+
 #Load data ----------------
 fishtkt <- read_excel(path = "./data/TannerFishTicketData_2017.xlsx", sheet = 1)
 logbook <- read_excel(path = "./data/TannerLogbookData_2017.xlsx", sheet = 1)
-
+statarea <- read.csv("./data/area_stat_areas.csv")
 ## logbook data by day of year
 
+## data manipulation -------
 # need to add survey area 3 designation - see "All logbook data_17" spreadsheet
 # need to convery effort date to day of year
-
 logbook %>% select(Year = YEAR, effort.date = EFFORT_DATE, District = DISTRICT, 
                    Sub.district = SUB_DISTRICT, ADFG_NO, pots = NUMBER_POTS_LIFTED, 
                    numbers = TARGET_SPECIES_RETAINED) -> logbook1 
 
-logbook1 %>% mutate(day = strftime(effort.date, format = "%j")) 
+logbook1 %>% mutate(day = strftime(effort.date, format = "%j")) %>% 
+  mutate(stat.area = paste(District,"-", Sub.district)) -> logbook1
+# add survey area 3 designation
+logbook1 %>% left_join(statarea) -> logbook2
 
+## determine cumulative pots ordered by day
+logbook2 %>% arrange(day) %>% mutate(cum.pots = cumsum(pots)) -> logbook2
+
+logbook2 %>% filter(cum.pots <= 12521) -> logbook2.std
