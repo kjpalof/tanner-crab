f675f8f32de6381482b7265cec09018e5595f962
Katie Palof <katie.palof@alaska.gov>
2018-12-06 14:04:46 -0900

    look at tanner harvest over time in each survey area. initial file.

---
+++ code/harvest_over_time.R
@@ -0,0 +1,12 @@
+# K.Palof
+# 12-6-18
+
+# Look at Tanner crab harvest over time in each managment area.
+# Total pounds and percentage of mature biomass in that area.
+
+## Data needs:
+# 1) Harvest over time
+# 2) Biomass estimate from each area from most recent year's CSAs - currently from 2018 survey
+
+# load ----
+source('./code/helper.R')
