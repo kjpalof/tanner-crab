e8c0a9477e54b810159bed67a4333e6ed39bde05
Katie Palof <katie.palof@alaska.gov>
2018-12-06 15:59:20 -0900

    tanner crab harvest with biomass plot ideas.

--- code/harvest_over_time.R
+++ code/harvest_over_time.R
@@ -10,3 +10,33 @@
 
 # load ----
 source('./code/helper.R')
+
+
+# data ----
+# obtained from project - crab_data_processing - in results folder - has fish ticket totals assigned with the
+# survey area that they correspond to - also includes confidentiality. 
+# see code file from the above project 'tanner_harvest.R'
+harvest <- read.csv('./data/tanner_comm_catch_97_2018_confid.csv')
+biomass <- read.csv('./data/tanner_2018_biomassmodel.csv')
+
+# data manipulation -----
+biomass %>% 
+  select(Year, Mature, survey.area = Area) %>% 
+  left_join(harvest) %>% 
+  select(Year, Mature, survey.area, Season, pounds, confidential) %>% 
+  mutate(hrate = pounds/Mature, hrate2 = round(hrate, 2)*100)-> data_combine
+
+# by area - harvest over time ----------
+harvest %>% 
+  filter(survey.area != "Other") %>% 
+  ggplot(aes(Year, pounds)) +
+  geom_point() +
+  facet_wrap(~survey.area)
+
+# by area - harvest with labels ------
+data_combine %>% 
+  ggplot(aes(x = Year, y = pounds)) +
+  geom_bar(stat = "identity") +
+  geom_text(aes(label = hrate2), vjust = -0.3, size = 3.5) +
+  geom_line(aes(x = Year, y = Mature)) +
+  facet_wrap(~survey.area)
