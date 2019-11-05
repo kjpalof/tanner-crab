e5f4ff0222c037ce4bc42b18a341eec71d317e60
kjpalof <katie.palof@alaska.gov>
2019-11-05 09:52:49 -0900

    updates for 2019.  Pulled all logbook data together so no need for current year calcs seperate

--- code/harvest_stdcpue.R
+++ code/harvest_stdcpue.R
@@ -1,8 +1,9 @@
-# K.Palof   11-8-18
+# K.Palof   11-8-18 / 11-5-19
 # calculationg of annual harvest and STD cpue for tanner crab fishery
-# Year: 2017-2018
-# results are currently graphed in: sigma plot 'Tanner SEAK Standardized CPUE_2017.JNB'
-# Move results to being graphed here.
+# Year: 2019-2020
+# result were previously graphed in: sigma plot 'Tanner SEAK Standardized CPUE_2017.JNB'
+
+# these results are used to create figures in SE_crab_assessments - more here **FIX**
 
 # Objective: harvest and std cpue for tanner crab current season
 
@@ -20,12 +21,12 @@ theme_set(theme_bw(base_size=12,base_family='Times New Roman')+
             theme(panel.grid.major = element_blank(),
                   panel.grid.minor = element_blank()))
 #Load data ----------------
-fishtkt <- read.csv("./data/Tanner_Detailed Fish Tickets_85_18.csv")
+fishtkt <- read.csv("./data/Tanner_Detailed Fish Tickets_ALL_years.csv")
 #logbook <- read_excel(path = "./data/TannerLogbookData_2017.xlsx", sheet = 1)
-logbook <- read_excel(path = "./data/2018 Tanner Logbook Data.xlsx", sheet = "AlexData")
+logbook <- read_excel(path = "./data/2019 Tanner Logbook Data.xlsx", sheet = "AlexData")
 statarea <- read.csv("./data/area_stat_areas.csv")
-logbook_all <- read_excel(path = "./data/All_logbook_data_17.xlsx", sheet = "r_input") # from ALEX not in OCEAN AK
-cur_yr = 2018
+logbook_all <- read_excel(path = "./data/All_logbook_tanner.xlsx", sheet = "AlexData") # from ALEX not in OCEAN AK
+cur_yr = 2019
 
 ## data manipulation -------
 
@@ -51,8 +52,8 @@ logbook1 %>%
 
 ## all years -------------
 # add current years data 
-logbook_all %>% 
-  bind_rows(logbook) -> logbook_all
+#logbook_all %>% 
+#  bind_rows(logbook) -> logbook_all
 
 # need to convery effort date to day of year
 logbook_all %>% select(Year = YEAR, effort.date = EFFORT_DATE, District = DISTRICT, 
@@ -74,7 +75,7 @@ logbook_all1 %>%
   summarise(avg.cpue = mean(cpue, na.rm = TRUE), 
             se = sd(cpue, na.rm = TRUE)/sqrt(length(cpue))) %>% 
   as.data.frame() %>% 
-  write_csv('./results/std_commericial_cpue.csv')
+  write_csv(paste0('./results/std_commericial_cpue' , cur_yr, '.csv'))
 
 
 
