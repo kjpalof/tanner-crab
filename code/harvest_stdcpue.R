195672634d1382511c1620aa2d7ec0f11161c2d3
kjpalof <katie.palof@alaska.gov>
2020-11-15 13:08:51 -0900

    updates for 2020.  Need to keep historic file and add to it...make this better for annual updating

--- code/harvest_stdcpue.R
+++ code/harvest_stdcpue.R
@@ -1,6 +1,7 @@
-# K.Palof   11-8-18 / 11-5-19
+# K.Palof   11-8-18 / 11-5-19 / 11-15-20
+
 # calculationg of annual harvest and STD cpue for tanner crab fishery
-# Year: 2019-2020
+# Year: 2020 - 2021
 # result were previously graphed in: sigma plot 'Tanner SEAK Standardized CPUE_2017.JNB'
 
 # these results are used to create figures in SE_crab_assessments - more here **FIX**
@@ -20,13 +21,16 @@ windowsFonts(Times=windowsFont("TT Times New Roman"))
 theme_set(theme_bw(base_size=12,base_family='Times New Roman')+ 
             theme(panel.grid.major = element_blank(),
                   panel.grid.minor = element_blank()))
+
+# global -----
+cur_yr = 2020
+
 #Load data ----------------
-fishtkt <- read.csv("./data/Tanner_Detailed Fish Tickets_ALL_years.csv")
+fishtkt <- read.csv(paste0('./data/Tanner_Detailed Fish Tickets_ALL_years_', cur_yr,'.csv'))
 #logbook <- read_excel(path = "./data/TannerLogbookData_2017.xlsx", sheet = 1)
-logbook <- read_excel(path = "./data/2019 Tanner Logbook Data.xlsx", sheet = "AlexData")
+logbook <- read_excel(path = paste0('./data/', cur_yr,' Tanner Logbook Data.xlsx'), sheet = "AlexData")
 statarea <- read.csv("./data/area_stat_areas.csv")
 logbook_all <- read_excel(path = "./data/All_logbook_tanner.xlsx", sheet = "AlexData") # from ALEX not in OCEAN AK
-cur_yr = 2019
 
 ## data manipulation -------
 
@@ -64,9 +68,12 @@ logbook_all %>% select(Year = YEAR, effort.date = EFFORT_DATE, District = DISTRI
 logbook_all1 %>% 
   mutate(day = strftime(effort.date, format = "%j")) -> logbook_all1
 
+## add years since 2019 since that's all I have in all logbook data...need to **fix** this.
+logbook_all1 %>% 
+  bind_rows(logbook1) -> logbook_all_cur
 ### std cpue -----------------
 ## determine cumulative pots ordered by day
-logbook_all1 %>% 
+logbook_all_cur %>% 
   arrange(day) %>% 
   group_by(Year) %>% 
   arrange(day) %>% 
